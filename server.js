3792661829e7734d542db9e96a99b3eda616e419
thagen15 <thagen@wpi.edu>
2017-10-05 13:44:01 -0400

    added firebase

--- server.js
+++ server.js
@@ -2,8 +2,57 @@ var http = require('http')
   , qs   = require('querystring')
   , fs   = require('fs')
   , url  = require('url')
-  , sqlite3 = require('sqlite3')
+  , firebase = require("firebase")
   , port = 8080
+
+
+  var config = {
+     apiKey: "AIzaSyBQxD4DSfJ31NEgQv8fUJoEmEhl5yq5JZo",
+     authDomain: "cs4241-billiards.firebaseapp.com",
+     databaseURL: "https://cs4241-billiards.firebaseio.com",
+     projectId: "cs4241-billiards",
+     storageBucket: "cs4241-billiards.appspot.com",
+     messagingSenderId: "622381521618"
+   };
+   firebase.initializeApp(config);
+
+// writeUserData('123', 'lane', 'l@a.a', 'imgurl')
+
+function writeUserData(username, name, exam1scores, exam2scores) {
+    firebase.database().ref('users/' + username).set({
+          username: username,
+          name: name,
+          f1: exam1scores[0],
+          f2: exam1scores[1],
+          f3: exam1scores[2],
+          f4: exam1scores[3],
+          f5: exam1scores[4],
+          f6: exam1scores[5],
+          f7: exam1scores[6],
+          f8: exam1scores[7],
+          ball_control: f2+f3+f4+f5+f7,
+          accuracy: f1+f6,
+          positioning: f8,
+          exam1total: ball_control+accuracy+positioning,
+          s1: exam2scores[0],
+          s2: exam2scores[1],
+          s3: exam2scores[2],
+          s4: exam2scores[3],
+          s5: exam2scores[4],
+          s6: exam2scores[5],
+          s7: exam2scores[6],
+          s8: exam2scores[7],
+          s9: exam2scores[8],
+          s10: exam2scores[9],
+          complex_situation_ability: s1+s2+s3+s4,
+          safe_ability: s5,
+          special_shot_ability: s6+s7+s8+s9,
+          break_ability: s10,
+          exam2total: complex_situation_ability+safe_ability+break_ability+break_ability
+          exam_total: exam1total+exam2total
+        });
+}
+
 
 var server = http.createServer(function (req, res) {
   var uri = url.parse(req.url)
