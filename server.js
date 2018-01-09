e2bc9744b06512e840ec3e408a920b5085d9a35f
Rayyan <rakhan@wpi.edu>
2018-01-09 14:15:25 -0500

    shortened date

--- server.js
+++ server.js
@@ -416,7 +416,8 @@ function writeUserData(username,examScores,combinedScores,whichExam) {
     var date = new Date();
     var day = date.getDate();
     var month = date.getMonth() + 1;
-    var year = date.getFullYear();
+    var fullYear = "" + date.getFullYear();
+    var year = fullYear.substr(2);
     if(hasName){
 
       switch(whichExam){
