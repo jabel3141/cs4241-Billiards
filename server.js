77b58f23e54b3fff4ecdc8433e1c61cc2fbf97e3
Rayyan <xboxer221@live.com>
2018-02-17 20:45:36 -0500

    removed notices

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ firebase.initializeApp(config);
 
 var server = http.createServer(function (req, res) {
   var uri = url.parse(req.url)
-  // Note we no longer have an index.html file, but we handle the cases since that's what the browser will request
-  // You'll need to modify the below to account for POSTs
+
   switch( uri.pathname ) {
     case '/':
     sendFile(res, 'index.html')
